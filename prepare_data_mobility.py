02a5c008218c128463afd79f5b1bc5d369d8a244
czechoa <andrzej.czechu.ja@gmail.com>
2021-04-02 10:37:30 +0200

    code clean up

--- prepare_data_mobility.py
+++ prepare_data_mobility.py
@@ -3,7 +3,7 @@ from preparingData import get_combined_data, get_cols_with_no_nans
 
 train_data_path = 'data/2020_PL_Region_Mobility_Report.csv'
 i = range(2, 15)
-data = pd.read_csv(train_data_path, usecols=i)=True
+data = pd.read_csv(train_data_path, usecols=i)
 data = data.drop(['metro_area', 'census_fips_code'], axis=1)
 print(data.shape)
 print(data.columns.values)
@@ -20,4 +20,3 @@ print(data.shape)
 data = data.groupby(by=['sub_region_1', 'date']).mean().reset_index()
 # %%
 data['residential_percent_change_from_baseline']
-
