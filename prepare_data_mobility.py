5fd32379cc79e275d8917a2cb07868b8a8381d8c
czechoa <andrzej.czechu.ja@gmail.com>
2021-05-27 17:10:12 +0200

    add Poland to prepare_data_mobility.py

--- prepare_data_mobility.py
+++ prepare_data_mobility.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import numpy as np
 
+
 def prepare_date_mobility(data_path):
+    # SUB_REGION_ 2 = 2
     i = list(range(8, 15))
-    i.append(2)
-    i.append(5)
+    i.extend((2, 5))
+
     data: pd.DataFrame = pd.read_csv(data_path, usecols=i)
+    data_Poland = data[data['sub_region_1'].isna()].copy()
+    # data_Poland.loc[:, 'sub_region_1'] = 'Poland'
+    data_Poland['sub_region_1'] = 'Poland'
+
     data = data[data['iso_3166_2_code'].notna()]
+    data = data.append(data_Poland)
     data = data.drop(columns='parks_percent_change_from_baseline')
     data = data.interpolate()
     # data = day_of_the_week_for_all_regions(data)
@@ -15,11 +22,11 @@ def prepare_date_mobility(data_path):
 
 def day_of_the_week_for_all_regions(data: pd.DataFrame):
     number_of_region = len(data['sub_region_1'].unique())
-    number_day = int(data.shape[0]/number_of_region)
+    number_day = int(data.shape[0] / number_of_region)
 
-    day_of_the_week = [x % 7 + 1 for x in range(1,number_day + 1)]
+    day_of_the_week = [x % 7 + 1 for x in range(1, number_day + 1)]
     day_of_the_week_all = np.tile(day_of_the_week, number_of_region)
-    data.insert(3,'day of the week', day_of_the_week_all)
+    data.insert(3, 'day of the week', day_of_the_week_all)
     return data
 
 
@@ -38,11 +45,11 @@ def get_prepared_data_mobility():
     data_2021 = prepare_date_mobility(train_data_path_1)
     data_all = merge_data_2020_2021(data_2020, data_2021)
     data_all = day_of_the_week_for_all_regions(data_all)
-    data_all = data_all.drop(columns=data_all.columns[1])
+    data_all = data_all.drop(columns='iso_3166_2_code')
 
     return data_all
 
 
 # %%
-# data = get_prepared_data_mobility()
+data:pd.DataFrame = get_prepared_data_mobility()
 
