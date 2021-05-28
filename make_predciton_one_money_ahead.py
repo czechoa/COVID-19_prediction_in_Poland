e41855f89a7f9e2fe18c79d55158273f1d95e2df
czechoa <andrzej.czechu.ja@gmail.com>
2021-05-28 13:40:24 +0200

    clean up (make def  make_prediction_one_mounth_ahead_for_train_all)

--- make_predciton_one_money_ahead.py
+++ make_predciton_one_money_ahead.py
@@ -15,63 +15,44 @@ def next_day(date: str):
     modified_date = date + timedelta(days=1)
     return datetime.strftime(modified_date, "%Y-%m-%d")
 
-last_day_train = '2021-03-20'
-layers_n = 2
+def make_prediction_one_mounth_ahead_for_train_all(data_merge, train_all, period_of_time, last_day_train):
 
-period_of_time = 21
+    result_all = pd.DataFrame(columns=['date', 'region', 'Liczba zajętych respiratorów (stan ciężki)',
+                                       'prediction'])
+    result_all_err = pd.DataFrame()
+    day = next_day(last_day_train)
+    for day_ahead_to_predict in range(1,31):
+        train, target = get_train_target(data_merge, train_all, period_of_time, day_ahead_to_predict)
 
-data_merge = get_merge_data_from_to(last_day = last_day_train)
-# data_merge = data_merge[data_merge["region"] != 'POLSKA']
-train_all = reshape_data_merge_to_get_train_period_of_time_history(data_merge, period_of_time)
-test_to_predict = make_date_to_prediction(train_all)
+        # train, test_to_predict = standardScaler(train, test_to_predict, input_scaler=MinMaxScaler())
+        # train = avarage_train_from_n_days(train, 3)
 
-result_all = pd.DataFrame(columns=['date', 'region', 'Liczba zajętych respiratorów (stan ciężki)',
-                                   'prediction'])
-result_all_err = pd.DataFrame()
-day = last_day_train
-for day_ahead_to_predict in range(1,31):
-    train, target = get_train_target(data_merge, train_all, period_of_time, day_ahead_to_predict)
+        make_all(train, target)
+        submission = make_submission(test_to_predict, day_ahead_to_predict)
+        clear_model()
 
-    # train, test_to_predict = standardScaler(train, test_to_predict, input_scaler=MinMaxScaler())
-    # train = avarage_train_from_n_days(train, 3)
-    day = next_day(day)
-    make_all(train, target)
-    submission = make_submission(test_to_predict, day_ahead_to_predict)
-    clear_model()
+        day = next_day(day)
+        # submission = add_prediction_to_submission(test_sc, submission, day_ahead_to_predict)
 
-    # submission = add_prediction_to_submission(test_sc, submission, day_ahead_to_predict)
+        submission = submission.reset_index()
+        test_ahead: pd.DataFrame = get_test_respiration(date=day)
 
-    submission = submission.reset_index()
-    test_ahead: pd.DataFrame = get_test_respiration(date=day)
-
-
-
-
-    submission.rename(columns={submission.columns[0]: test_ahead.columns[1], submission.columns[2]: 'prediction'},
-                      inplace=True)
-    result = pd.merge(test_ahead, submission.drop(columns=submission.columns[1]), on=test_ahead.columns[1])
-    result_err = result.iloc[:, :2]
-    result_err['subtract'] = result.iloc[:, -2].astype(float) - result.iloc[:, -1].astype(float)
-    result_err['relative error in %'] = abs(result_err.loc[:, 'subtract'] / result.iloc[:, -1].astype(float)) * 100
-    result_all = result_all.append(result, ignore_index=True)
-    result_all_err = result_all_err.append(result_err, ignore_index=True)
+        submission.rename(columns={submission.columns[0]: test_ahead.columns[1], submission.columns[2]: 'prediction'},
+                          inplace=True)
+        result = pd.merge(test_ahead, submission.drop(columns=submission.columns[1]), on=test_ahead.columns[1])
+        result_err = result.iloc[:, :2]
+        result_err['subtract'] = result.iloc[:, -2].astype(float) - result.iloc[:, -1].astype(float)
+        result_err['relative error in %'] = abs(result_err.loc[:, 'subtract'] / result.iloc[:, -1].astype(float)) * 100
+        result_all = result_all.append(result, ignore_index=True)
+        result_all_err = result_all_err.append(result_err, ignore_index=True)
     #
     # norm_2 = np.linalg.norm(result_err['relative error in %'], ord=2)
     # print(norm_2)
+    result_all = result_all.sort_values(by=['region', 'date'])
+    result_all_err = result_all_err.sort_values(by=['region', 'date'])
 
-regions = result.loc[:, 'region'].unique()
-data_merge_from_to = get_merge_data_from_to('2021-03-01', '2021-05-01')
+    return result_all, result_all_err
 
-zachodnie = data_merge_from_to.loc[data_merge_from_to['region'] == 'ZACHODNIOPOMORSKIE']
-days_from_to = pd.to_datetime(zachodnie.loc[:, 'date'].values, format='%Y-%m-%d')
-result_all_err = result_all_err.sort_values(by=['region', 'date'])
-
-
-# %%
-
-
-# %%
-# train_av = avarage_train_from_n_days(train, 3)
 def plot_prediction_to_Poland(result_all_f, data_merge_from_to_f):
     fig, ax = plt.subplots()
 
@@ -79,6 +60,10 @@ def plot_prediction_to_Poland(result_all_f, data_merge_from_to_f):
     region_merge = data_merge_from_to_f.loc[data_merge_from_to_f['region'] == "POLSKA"]
 
     y = region_merge.iloc[:, -1].astype(float)
+
+    zachodnie = data_merge_from_to.loc[data_merge_from_to['region'] == 'ZACHODNIOPOMORSKIE']
+    days_from_to = pd.to_datetime(zachodnie.loc[:, 'date'].values, format='%Y-%m-%d')
+
     plt.plot(days_from_to, y, label="reality")
 
     days = pd.to_datetime(region_prd.iloc[:, 0], format='%Y-%m-%d')
@@ -94,20 +79,26 @@ def plot_prediction_to_Poland(result_all_f, data_merge_from_to_f):
     plt.grid()
     plt.legend(loc='lower left')
     plt.show()
-    fig.savefig("results/Poland predition when you learn from sum")
+    # fig.savefig("results/Poland predition when you learn from sum")
 
 # %%
+last_day_train = '2021-03-20'
+period_of_time = 21
+
+data_merge = get_merge_data_from_to(last_day = last_day_train)
+# data_merge = data_merge[data_merge["region"] != 'POLSKA']
+train_all = reshape_data_merge_to_get_train_period_of_time_history(data_merge, period_of_time)
+test_to_predict = make_date_to_prediction(train_all)
+result_all, result_all_err = make_prediction_one_mounth_ahead_for_train_all(data_merge,train_all,period_of_time,last_day_train)
+data_merge_from_to = get_merge_data_from_to('2021-03-01', '2021-05-01')
+
 plot_prediction_to_Poland(result_all,data_merge_from_to)
 
-# %%
-Poland_df = data_merge[data_merge['region'] == 'POLSKA']
-train_all_Poland = train_all[train_all[0] == 'POLSKA']
-Poland_target = target[target.index.get_level_values(1) == 'Poland']
+
 # %%
 def plot_prediction_for_ech_region():
     fig, ax = plt.subplots(8,2 , figsize=(60, 30))
     fig.subplots_adjust(hspace = .5, wspace=.001)
-
     axs = ax.ravel()
     for i in range(len(regions)):
 
