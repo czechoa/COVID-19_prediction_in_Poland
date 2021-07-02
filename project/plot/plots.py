af1c50ebb160bf21d7bdad34f463ef0e20bf4d45
czechoa <andrzej.czechu.ja@gmail.com>
2021-07-02 17:53:00 +0200

    remove function for_each_region_single_plot

--- project/plot/plots.py
+++ project/plot/plots.py
@@ -32,34 +32,6 @@ def plot_prediction_to_Poland_from_results(result_all_list: list, labels: list, 
     fig.savefig(path)
 
 
-def for_each_region_single_plot(result_all_list: list, labels: list, data_merge_from_to_f: pd.DataFrame):
-    fig, ax = plt.subplots()
-
-    date = data_merge_from_to_f['date'].unique()
-    days_from_to = pd.to_datetime(date, format='%Y-%m-%d')
-    for region in data_merge_from_to_f['region'].unique():
-        region_merge = data_merge_from_to_f.loc[data_merge_from_to_f['region'] == region]
-        y = region_merge.iloc[:, -1].astype(float)
-        plt.plot(days_from_to, y, label="reality")
-
-        for result_all_f, label in zip(result_all_list, labels):
-            region_prd: pd.DataFrame = result_all_f.loc[result_all_f['region'] == region]
-            days = pd.to_datetime(region_prd.iloc[:, 0], format='%Y-%m-%d')
-            y = region_prd['prediction'].astype(float).values
-            plt.plot(days, y, label=label)
-
-        # plt.scatter(days_from_to[0], y)
-        # plt.annotate("Point 1", (1, 4))
-
-        ax.set(xlabel="Date",
-               ylabel="Engaged respiration",
-               )
-        plt.title(region + ' prediction engaged respiration')
-        plt.gcf().autofmt_xdate()
-        plt.grid()
-        plt.legend(loc='lower left')
-        plt.show()
-
 
 def subplot_prediction_for_all_region(result_all_list: list, labels: list, data_merge_from_to_f: pd.DataFrame):
     regions = result_all_list[0]['region'].unique()
