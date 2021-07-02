5a15cb1776e945ea8313aa726551c760402602df
czechoa <andrzej.czechu.ja@gmail.com>
2021-07-02 17:51:07 +0200

    remove funtion make error plots from prediction 1,3,7 back mean

--- project/plot/relative_error_plot.py
+++ project/plot/relative_error_plot.py
@@ -3,17 +3,6 @@ import pandas as pd
 from project.plot.plots import subplot_relative_error_for_all_region, plot_averaged_relative_error_for_all_region, \
     plot_relative_error_for_Polska
 
-
-def make_plots_relative_error_for_regions_from_list_predictions():
-    all_prediction_list = pd.read_csv('results/csv/all_prediction_for_region.csv')
-    prediction = all_prediction_list[all_prediction_list['avarage from n days back'] == 1]
-    prediction['relative_error_%'] = 100 * abs(
-        prediction['Liczba zajętych respiratorów (stan ciężki)'] - prediction['prediction']) / prediction[
-                                         'Liczba zajętych respiratorów (stan ciężki)']
-    subplot_relative_error_for_all_region(prediction)
-    plot_averaged_relative_error_for_all_region(prediction)
-
-
 def make_plots_relative_error_for_regions_with_augmentation(prediction = None):
     if prediction is not None:
         prediction = pd.read_csv('results/csv/prediction_for_region_with_data_augmentation.csv')
