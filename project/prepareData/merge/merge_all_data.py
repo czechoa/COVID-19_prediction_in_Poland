b72c3aa6ebc69465b3db6662ee0a258d69cef4cf
czechoa <andrzej.czechu.ja@gmail.com>
2021-07-02 12:21:37 +0200

    code cleanup

--- project/prepareData/merge/merge_all_data.py
+++ project/prepareData/merge/merge_all_data.py
@@ -3,20 +3,19 @@ from project.prepareData.augmentation.data_augmentation import data_augmentation
 from project.prepareData.prepare_data_area_population import preparing_data_area_population_regions
 import pandas as pd
 
-def get_all_merge_data_from_to(first_day: str = None, last_day='2021-04-04'):
 
+def get_all_merge_data_from_to(first_day: str = None, last_day='2021-04-04'):
     merge_data = get_merge_data_from_to(first_day, last_day)
     merge_from_to = merge_area_population(merge_data)
     merge_from_to = data_augmentation(merge_from_to)
 
     return merge_from_to
 
-def merge_area_population(merge_data: pd.DataFrame):
+
+def merge_area_population(merge_data: pd.DataFrame, attribute_dsc=3):
     region_area_population: pd.DataFrame = preparing_data_area_population_regions()
 
-    merge_dsc: pd.DataFrame = merge_data.iloc[:, :3].merge(region_area_population, on='region')
-    merge_all = merge_dsc.merge(merge_data, on=list(merge_data.columns.values[0:3]))
+    merge_dsc: pd.DataFrame = merge_data.iloc[:, :attribute_dsc].merge(region_area_population, on='region')
+    merge_all = merge_dsc.merge(merge_data, on=list(merge_data.columns.values[0:attribute_dsc]))
 
     return merge_all
-# %%
-a = get_all_merge_data_from_to(last_day='2021-05-01')
