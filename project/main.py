a5d60a29688a7001da76c1c441492d3580832d4f
czechoa <andrzej.czechu.ja@gmail.com>
2021-07-03 16:27:14 +0200

    make prediction for 30 days ahead

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
 from project.prediction.make_prediction_and_plots import make_prediction_and_subplot_for_all_regions, \
     make_plots_relative_error_for_regions
 
-result = make_prediction_and_subplot_for_all_regions(day_ahead=2,period_of_time=7)
+result = make_prediction_and_subplot_for_all_regions(day_ahead=30,period_of_time=7)
 make_plots_relative_error_for_regions(prediction = result)
 
 
