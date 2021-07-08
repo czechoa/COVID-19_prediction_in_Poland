f7a83163ee255a3623a35fb145ace6b440c8574a
czechoa <andrzej.czechu.ja@gmail.com>
2021-07-08 15:30:11 +0200

    make prediction  in run  ( to improve scaling)

--- LSTM_Neural_Network_for_Time_Series_Prediction/run.py
+++ LSTM_Neural_Network_for_Time_Series_Prediction/run.py
@@ -33,15 +33,14 @@ def plot_results_multiple(predicted_data, true_data, prediction_len):
     plt.show()
 
 df = pd.read_csv('LSTM_Neural_Network_for_Time_Series_Prediction/data/data_Poland_to_2021_05.csv')
-data_df = df.iloc[:,[-1]]
+data_df = df.iloc[:,[-2,-1]]
 data_desc = data_df.describe()
-data = data_df.values
+data_org = data_df.values
 
-data_nor = (data - data_desc.loc['mean'].values)/ (data_desc.loc['max'].values - data_desc.loc['min'].values)
-# data_nor = (data - data_desc.loc['mean'])/ (data_desc.loc['max'] - data_desc.loc['min'])
+data_nor = (data_org - data_desc.loc['mean'].values)/ (data_desc.loc['max'].values - data_desc.loc['min'].values)
+# data_nor = (data_org - data_desc.loc['mean'])/ (data_desc.loc['max'] - data_desc.loc['min'])
 
 df_ns = pd.DataFrame(columns= data_df.columns, data = data_nor)
-
 df_ns.to_csv('LSTM_Neural_Network_for_Time_Series_Prediction/data/data_Poland_to_2021_05_ns.csv', index= False)
 
 configs = json.load(open('LSTM_Neural_Network_for_Time_Series_Prediction/config.json', 'r'))
@@ -72,20 +71,25 @@ x_test, y_test = data.get_test_data(
 )
 
 predictions = model.predict_point_by_point(x_test)
+
 predictions = np.array( predictions) * (data_desc.loc['max'][-1] -  data_desc.loc['min'][-1] ) + data_desc.loc['mean'][-1]
-
-# predictions = predictions * (data_org.max() - data_org.min()) + data_org.mean()
-# y_test = y_test * (data_org.max() - data_org.min()) + data_org.mean()
-# # data.denormalise_windows(y_test)
-# # plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
-predictions_full:list = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
-predictions_sc = np.array( predictions_full) * (data_desc.loc['max'][-1] -  data_desc.loc['min'][-1] ) + data_desc.loc['mean'][-1]
-
-
 y_test_sc = np.array( y_test) * (data_desc.loc['max'][-1] -  data_desc.loc['min'][-1] ) + data_desc.loc['mean'][-1]
 
 
-plot_results(predictions_sc,y_test_sc)
-plot_results(predictions,y_test_sc)
+# predictions = predictions * (data_org.max() - data_org.min()) + data_org.mean()
+# y_test = y_test * (data_org.max() - data_org.min()) + data_org.mean()
 
+plot_results(predictions,y_test_sc)
+# %%
+# # plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+
+predictions_full:list = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
+predictions_sc = np.array( predictions_full) * (data_desc.loc['max'][-1] -  data_desc.loc['min'][-1] ) + data_desc.loc['mean'][-1]
+# y_test_sc = np.array( y_test) * (data_desc.loc['max'][-1] -  data_desc.loc['min'][-1] ) + data_desc.loc['mean'][-1]
+
+# plot_results(predictions_full,y_test)
+
+plot_results(predictions_sc,y_test_sc)
+# %%
+plot_results(predictions,y_test_sc)
 
