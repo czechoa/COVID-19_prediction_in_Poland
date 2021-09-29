627a7c75d862a26365f45e88631963e54ca700b0
czechoa <andrzej.czechu.ja@gmail.com>
2021-09-29 12:14:51 +0200

    adding a comment

--- RNN/main.py
+++ RNN/main.py
@@ -1,5 +1,5 @@
 from RNN.prediction.make_prediction_and_plots import make_prediction_and_subplot_for_all_regions
-# %%
+# %% to 2021-03-25
 result = make_prediction_and_subplot_for_all_regions(last_day_train= '2021-03-25', day_ahead=21, period_of_time_learning=7)
 
 # %% current
