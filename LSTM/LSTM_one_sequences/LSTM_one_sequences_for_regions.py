f3ce85d2f0cef2f39530ddc3d75eceb7f3870ffe
czechoa <andrzej.czechu.ja@gmail.com>
2021-10-02 14:21:21 +0200

    make the data_processor.py as class ( in progress, doesn't work)

--- LSTM/LSTM_one_sequences/LSTM_one_sequences_for_regions.py
+++ LSTM/LSTM_one_sequences/LSTM_one_sequences_for_regions.py
@@ -3,10 +3,10 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.models import Sequential
 
-from LSTM.LSTM_one_sequences.data_processor import read_data_only_Poland, make_trainX_trainY, inverse_transform_train, \
+from LSTM.LSTM_one_sequences.data_processor import make_trainX_trainY, inverse_transform_train, \
     inverse_transform_test
 from LSTM.LSTM_one_sequences.plot import Plot
-
+import pandas as pd
 
 def create_and_fit_model(_trainX, _trainY):
     features = _trainX.shape[2]
@@ -47,10 +47,23 @@ def make_prediction_future(model, train, trainY, test):
     future_array = future_array.reshape(future_array.shape[0], features)
     return future_array, predictions_train
 
+def read_data_regions():
+    data_merge = pd.read_csv('data/data_lstm/data_all_with_one_hot_encode.csv')
+    data_merge["region"].replace({"ŚŚ_average": "ŚŚŚ_average"}, inplace=True)
+    data_merge = data_merge.sort_values(by=['region', 'date'])
+    return data_merge
+
+
+def read_data_only_Poland():
+    data_merge = pd.read_csv('data/data_lstm/merge_data_Poland.csv')
+    data_merge["region"].replace({"POLSKA": "ŚŚŚ_Poland"}, inplace=True)
+    data_merge = data_merge.sort_values(by=['region', 'date'])
+    return data_merge
+
 data_merge = read_data_only_Poland()
 split = 0.83
 
-train, test, trainX, trainY, _region, scaler = make_trainX_trainY(data_merge, split)
+train, test, trainX, trainY, region, scaler = make_trainX_trainY(data_merge, split)
 
 model = create_and_fit_model(trainX, trainY)
 trainX = trainX[-1:, :, :]
@@ -59,6 +72,6 @@ trainY = trainY[-1:, :, :]
 future_array, predictions_train = make_prediction_future(model, train, trainY, test)
 trainPredict, trainY = inverse_transform_train(scaler, predictions_train, trainY)
 testPredict, testY = inverse_transform_test(scaler, future_array, test)
-plot = Plot(_region,trainPredict,testPredict,split)
+plot = Plot(region, trainPredict, testPredict, split)
 plot.get_org_data_from_region_make_plot()
 
--- LSTM/LSTM_one_sequences/data_processor.py
+++ LSTM/LSTM_one_sequences/data_processor.py
@@ -1,98 +1,86 @@
 import numpy as np
-from pandas import read_csv
 from sklearn.preprocessing import MinMaxScaler
 
+class Data_processor:
+    def __init__(self, data,split):
+        self.split = split
+        self.data = data
+        self.x_full = np.array
+        self.y_full = np.array
+        self.columns_index = [-1]
 
-def create_dataset(_dataset, _look_back=1):
-    dataX, dataY = [], []
-    for i in range(len(_dataset) - _look_back):
-        a = _dataset[i:(i + _look_back), :]
-        dataX.append(a)
-        dataY.append(_dataset[i + _look_back, :])
-    return np.array(dataX), np.array(dataY)
+    def create_dataset(self, _dataset, _look_back=1):
+        dataX, dataY = [], []
+        for i in range(len(_dataset) - _look_back):
+            a = _dataset[i:(i + _look_back), :]
+            dataX.append(a)
+            dataY.append(_dataset[i + _look_back, :])
+        return np.array(dataX), np.array(dataY)
 
-
-def load_dataset(_region, _columns_index=[-2, -1]):
-    dataframe = _region.iloc[:, _columns_index]
-    dataset = dataframe.values
-    dataset = dataset.astype('float32')
-    return dataset
-
-
-def normalise_dataset(_dataset):
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    _dataset = scaler.fit_transform(_dataset.reshape(len(_dataset), _dataset.shape[1]))
-    return _dataset, scaler
-
-
-def split_into_train_and_test_sets(dataset, split=0.85):
-    train_size = int(len(dataset) * split)
-    test_size = len(dataset) - train_size
-    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
-    return train, test
+    def load_dataset(self, region):
+        dataframe = region.iloc[:,self.columns_index]
+        dataset = dataframe.values
+        dataset = dataset.astype('float32')
+        return dataset
 
 
-def reshape_train_to_one_sample(trainX, trainY):
-    trainX = np.reshape(trainX, (1, trainX.shape[0], trainX.shape[2]))
-    trainY = np.reshape(trainY, (1, trainY.shape[0], trainY.shape[1]))
-    return trainX, trainY
+    def normalise_dataset(self, dataset):
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        dataset = scaler.fit_transform(dataset.reshape(len(dataset), dataset.shape[1]))
+        return dataset, scaler
 
 
-def read_data_regions():
-    data_merge = read_csv('data/data_lstm/data_all_with_one_hot_encode.csv')
-    data_merge["region"].replace({"ŚŚ_average": "ŚŚŚ_average"}, inplace=True)
-    data_merge = data_merge.sort_values(by=['region', 'date'])
-    return data_merge
+    def split_into_train_and_test_sets(self, dataset):
+        train_size = int(len(dataset) * self.split)
+        # test_size = len(dataset) - train_size
+        train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+        return train, test
 
 
-def read_data_only_Poland():
-    data_merge = read_csv('data/data_lstm/merge_data_Poland.csv')
-    data_merge["region"].replace({"POLSKA": "ŚŚŚ_Poland"}, inplace=True)
-    data_merge = data_merge.sort_values(by=['region', 'date'])
-    return data_merge
+    def reshape_train_to_one_sample(self,trainX, trainY):
+        trainX = np.reshape(trainX, (1, trainX.shape[0], trainX.shape[2]))
+        trainY = np.reshape(trainY, (1, trainY.shape[0], trainY.shape[1]))
+        return trainX, trainY
+
+    def inverse_transform_train(self, scaler, predictions, trainY):
+        trainPredict = scaler.inverse_transform(self.reshape_back(predictions))
+        trainY = scaler.inverse_transform(self.reshape_back(trainY))
+        return trainPredict, trainY
 
 
-def inverse_transform_train(scaler, predictions, trainY):
-    trainPredict = scaler.inverse_transform(reshape_back(predictions))
-    trainY = scaler.inverse_transform(reshape_back(trainY))
-    return trainPredict, trainY
+    def inverse_transform_test(self, scaler, future_array, testY):
+        testPredict = scaler.inverse_transform(future_array)
+        testY = scaler.inverse_transform(testY)
+        return testPredict, testY
 
 
-def inverse_transform_test(scaler, future_array, testY):
-    testPredict = scaler.inverse_transform(future_array)
-    testY = scaler.inverse_transform(testY)
-    return testPredict, testY
+    def reshape_back(self,train_f: np.array):
+        train_f = train_f[0, :, :]
+        return train_f
 
 
-def reshape_back(train_f: np.array):
-    train_f = train_f[0, :, :]
-    return train_f
+    def make_trainX_trainY(self):
+        global x_full, y_full
+        first = True
 
+        for region_name in self.data['region'].unique()[0:]:
+            if region_name == 'POLSKA':
+                continue
+            region = self.data[self.data['region'] == region_name]
+            dataset = self.load_dataset(region)
+            dataset, scaler = self.normalise_dataset(dataset)
 
-def make_trainX_trainY(data, split):
-    first = True
-    x_full = np.array
-    y_full = np.array
-    columns_index = [-1]
+            train, test = self.split_into_train_and_test_sets(dataset)
+            trainX, trainY = self.create_dataset(train)
+            # testX, testY = create_dataset(test)
+            trainX, trainY = self.reshape_train_to_one_sample(trainX, trainY)
 
-    for region_name in data['region'].unique()[0:]:
-        if region_name == 'POLSKA':
-            continue
-        region = data[data['region'] == region_name]
-        dataset = load_dataset(region, columns_index)
-        dataset, scaler = normalise_dataset(dataset)
+            if first:
+                x_full = trainX
+                y_full = trainY
+                first = False
+            else:
+                x_full = np.concatenate((x_full, trainX), axis=0)
+                y_full = np.concatenate((y_full, trainY), axis=0)
 
-        train, test = split_into_train_and_test_sets(dataset, split=split)
-        trainX, trainY = create_dataset(train)
-        testX, testY = create_dataset(test)
-        trainX, trainY = reshape_train_to_one_sample(trainX, trainY)
-
-        if first:
-            x_full = trainX
-            y_full = trainY
-            first = False
-        else:
-            x_full = np.concatenate((x_full, trainX), axis=0)
-            y_full = np.concatenate((y_full, trainY), axis=0)
-
-    return train, test, x_full, y_full, region, scaler
+        return train, test, x_full, y_full, region, scaler
